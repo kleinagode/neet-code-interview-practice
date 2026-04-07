3be1dffe6dcca12af16d9ce62c4868cb3eaede11
Klei Nagode <162731839+kleinagode@users.noreply.github.com>
2026-04-07 14:29:41 -0500

    Add: products-of-array-discluding-self - submission-4

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-4.py
@@ -0,0 +1,25 @@
+# Optimal solition O(n) Prefix Postfix
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        prefix = [1] * n
+        postfix = [1] * n
+        res = [0] * n
+
+        # Build prefix array
+        for i in range(1, n):
+            prefix[i] = prefix[i-1] * nums[i-1]
+
+        # Build postfix array
+        for i in range(n-2, -1, -1):
+            postfix[i] = postfix[i+1] * nums[i+1]
+
+        # Build result
+        for i in range(n):
+            res[i] = prefix[i] * postfix[i]
+
+        return res
+        
+        
+        
